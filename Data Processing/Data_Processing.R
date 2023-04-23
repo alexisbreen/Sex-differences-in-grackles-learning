8c044af375faf8912f3437a6259bfbfa23404aa8
alexisbreen <alexisjuliebreen@gmail.com>
2023-04-23 13:42:50 +1100

    Data cleaning edits to catch further original oversights

--- Data Processing/Data_Processing.R
+++ Data Processing/Data_Processing.R
@@ -12,9 +12,9 @@
 library(tidyverse)
 
 #Load original data sheets
-dat_AZ <- read.csv(file.choose(), header = T) 
-dat_SB <- read.csv(file.choose(), header = T) 
-dat_WL <- read.csv(file.choose(), header = T) 
+dat_AZ <- read.csv(file.choose(), header = T) #Load Tempe_AZ_data
+dat_SB <- read.csv(file.choose(), header = T) #Load Santa_Barbara_CA_data
+dat_WL <- read.csv(file.choose(), header = T) #Load Woodland_CA_data
 
 #Select relevant columns
 d_AZ <- dat_AZ[ ,c(4,6,8,11)] 
@@ -98,7 +98,7 @@ which(is.na(d_AZ)) #Returns integer(0) i.e., R saying no NAs found in data sheet
 #Resume data entry error check for d_AZ
 Check_Entry_Errors(d_AZ)
 
-#Rows 46, 53, 454, 455, 656, 810 & 1773 flagged in error check, either b/c of a number jump or repeat in Trial column: jump, repeat, repeat, jump, jump, jump, jump, respectively.  
+#Rows 46, 53, 454, 455, 656, 810 & 1623 flagged in error check, either b/c of a number jump or repeat in Trial column: jump, repeat, repeat, jump, jump, jump, jump, respectively.  
 #Drop Trial column with errors, create new clean Trial column & recheck for errors
 d_AZ <- d_AZ[, -3] 
 d_AZ <- d_AZ %>% group_by(ID, Phase) %>% mutate(Trial = row_number())
@@ -157,12 +157,16 @@ for (i in 1: nrow(dat)) {
 #NOTE (VERY IMPORTANT!!!): Pina and Bunuelo birds below use a letter from the Latin alphabet - a Spanish n with a tilde: https://en.wikipedia.org/wiki/%C3%91#:~:text=%C3%91%2C%20or%20%C3%B1%20(Spanish%3A,%2D%20or%20lower%2Dcase%20N.
 #And this letter does not format correctly when uploading this sheet into R
 #So in order reproduce our data sheet, please replace whatever symbol - typically a question mark (?) - that R has used, with the correct Spanish n with a tilde
+
+#Our data sheets opened on some computers format these special characters differently - specifically, they show PiÃ±a and BuÃ±uelo
+#So we include these extra variants below
+
 dat$sex <- NA
 for(i in 1:nrow(dat)){
   if(dat$ID[i] == "Adobo" | dat$ID[i] == "Chalupa" | dat$ID[i] == "Memela" | dat$ID[i] == "Tapa" | 
      dat$ID[i] == "Yuca" | dat$ID[i] == "Cerveza" | dat$ID[i] == "Horchata" | dat$ID[i] == "Margarita" | 
      dat$ID[i] == "Michelada" | dat$ID[i] == "Alegria" | dat$ID[i] == "Cocinera" | dat$ID[i] == "Cutuy" | 
-     dat$ID[i] == "Flan" | dat$ID[i] == "Galandra" | dat$ID[i] == "Kel" | dat$ID[i] == "Piña" | #Make sure this bird has the proper Spanish n with a tilde: https://en.wikipedia.org/wiki/%C3%91#:~:text=%C3%91%2C%20or%20%C3%B1%20(Spanish%3A,%2D%20or%20lower%2Dcase%20N.
+     dat$ID[i] == "Flan" | dat$ID[i] == "Galandra" | dat$ID[i] == "Kel" | dat$ID[i] == "PiÃ±a" | dat$ID[i] == "Piña" | #Make sure this bird has the proper Spanish n with a tilde: https://en.wikipedia.org/wiki/%C3%91#:~:text=%C3%91%2C%20or%20%C3%B1%20(Spanish%3A,%2D%20or%20lower%2Dcase%20N.
      dat$ID[i] == "Tzanatl preciosa"){
     dat$sex[i] <- 1
   } else {
@@ -171,7 +175,7 @@ for(i in 1:nrow(dat)){
        dat$ID[i] == "Marisco" | dat$ID[i] == "Mofongo" | dat$ID[i] == "Mole" | dat$ID[i] == "Pizza" | 
        dat$ID[i] == "Pollito" | dat$ID[i] == "Queso" | dat$ID[i] == "Taco" | dat$ID[i] == "Taquito" | 
        dat$ID[i] == "Tomatillo" | dat$ID[i] == "Batido" | dat$ID[i] == "Jugo" | dat$ID[i] == "Refresco" | 
-       dat$ID[i] == "Tequila" | dat$ID[i] == "Ak'xi'" | dat$ID[i] == "Buñuelo" | #Make sure this bird has the proper Spanish n with a tilde: https://en.wikipedia.org/wiki/%C3%91#:~:text=%C3%91%2C%20or%20%C3%B1%20(Spanish%3A,%2D%20or%20lower%2Dcase%20N.
+       dat$ID[i] == "Tequila" | dat$ID[i] == "Ak'xi'" | dat$ID[i] == "BuÃ±uelo" | dat$ID[i] == "Buñuelo" | #Make sure this bird has the proper Spanish n with a tilde: https://en.wikipedia.org/wiki/%C3%91#:~:text=%C3%91%2C%20or%20%C3%B1%20(Spanish%3A,%2D%20or%20lower%2Dcase%20N.
        dat$ID[i] == "Camote" | dat$ID[i] == "Cuervo" | dat$ID[i] == "Dulce de Leche" | dat$ID[i] == "Helado" | 
        dat$ID[i] == "Kau" | dat$ID[i] == "Polvorones" | dat$ID[i] == "Tembleque" | dat$ID[i] == "Xunub" | 
        dat$ID[i] == "Zapote Negro"){
@@ -213,13 +217,16 @@ for(i in 1:nrow(dat)){
 
 #Original data has two different learning criterion applied by experimenters: 
 
-#(1) fixed-window method (core & middle pops), where birds had to make 17/20 correct choices, with a min. of 8 and 9 correct in either of the last two 10-trial blocks, 
-#which was assessed at the end of each block (e.g., 20, 30, 40); and which therefore doesn't always reflect the true trial to pass (by a maximum of 2 trials extra)
+#(1) fixed-window method (core & middle pops), where birds had to make 17/20 correct choices, with a min. of 8 and 9 correct across the last two 10-trial blocks, 
+#which was assessed at the end of each block (e.g., 20, 30, 40) 
+
 #(2) sliding-window method (edge pop), where birds had to make 17/20 correct choices, with a min. of 8 and 9 correct in either of the last two sets of 10 trials,
-#which was assessed at each trial rather than at the end of each 10-trial block. 
-#This second method of assessing learning is nonsensical: because a bird can meet criterion *before* making their choice.
-#For example, consider the following sequence of 20 incorrect (0) and correct (1) choices, broken into 10-trial sets: 0001111111 1111111110
-#Now at the start of the next trial, the bird will already pass, because the first set loses a zero and gains a 1: 0011111111 111111110?
+#which was assessed at each trial rather than at the end of each 10-trial block
+
+#Both methods of assessing learning are problematic, because (1) the fixed-window method doesn't always reflect the true trial to pass (by a maximum of 2 extra trials);
+#and (2) the sliding-window method can result in a bird meeting criterion *before* making a choice. 
+#For example, consider the following sequence of 20 incorrect (0) and correct (1) choices, broken into 10-trial sets: 0001111111 1111111110 (7/10) and (9/10) correct
+#Now at the start of the next trial, the bird will already pass, because the first set loses a zero and gains a 1: 0011111111 111111110? (8/10) and at least (8/10) correct no matter next choice
 
 #We want to examine learning across all birds using an informative and consistent method
 #Thus, we apply a straightforward 17/20 method to all birds, beginning at trial 17
@@ -256,7 +263,7 @@ for(i in 1:nrow(dat)){
 
 #Check all birds only have one criterion 1 - condition is met
 #There should be three 0s for criterion 1 in phase 2 for the three birds pulled early on (see above) - condition is met
-table(dat$ID, dat$Criterion, dat$sex,dat$Phase)
+table(dat$ID, dat$Criterion, dat$Phase)
 
 #Let's safety check our criterion assignment, by comparing it to cumulative counts 
 #First, we apply the sliding scale counts
@@ -290,33 +297,69 @@ for(i in 1:nrow(dat)){
   }
 }
 
-#As an aside regarding extra initial learning trials, these are already an inherent feature of core- and middle-population birds, because of the fixed-window method applied by the experimenter(s), detailed above.
-#And if we apply the sliding-window method originally used by experimenters to the edge-population data, we find that these birds also gained extra learning trials due to experimenter oversight
-#SW_Criterion of 0 = not passed; 1 = passing trial; 2 = extra learning trials, either in initial or reversal 
-dat$SW_Criterion <- NA
+#As an aside regarding extra learning trials,
+#let's examine how many birds in the core- and middle-populations gained extra learning trials via the fixed-window method
+
+#Fixed-window Criterion
+dat$FW_Criterion <- NA #0 = not passed; 1 = passing trial; either in initial or reversal 
 for(i in 1:nrow(dat)){
-  
-  dat$SW_Criterion[i] <- 0 #Baseline for all rows
-  
+  dat$FW_Criterion[i] <- 0
+  if(dat$Trial[i] >= 20 & dat$Trial[i]%%10 == 0){
+    if(sum(dat$Correct[(i - 19):i]) >= 17){
+      if((sum(dat$Correct[(i - 19):(i - 10)]) >= 8) & (sum(dat$Correct[(i - 9):i]) >= 8)){
+        dat$FW_Criterion[i] <- 1
+      }
+    }
+  }
+}
+
+#Compared to when they actually passed 
+dat$Actual_pass_diff <- NA #0 = not extra trials; 1 = 1 extra trial; 2 = 2 extra trials; either in intial or reversal
+for(i in 1:nrow(dat)){
+  if(dat$FW_Criterion[i] == 1){
+    if(sum(dat$Correct[(i - 19):i]) == 17){
+      dat$Actual_pass_diff[i] <- 0 
+    } else if(sum(dat$Correct[(i - 19):(i - 1)]) == 17){
+      dat$Actual_pass_diff[i] <- 1
+    } else if(sum(dat$Correct[(i - 19):(i - 2)]) == 17){
+      dat$Actual_pass_diff[i] <- 2 
+    } 
+  }
+}
+
+core_middle_oversight <- dat %>% filter(Population != 3) #Exclude edge birds
+table(core_middle_oversight$ID, core_middle_oversight$Actual_pass_diff, core_middle_oversight$Phase) #Returns 18 in initial (range: 1 - 2), 13 in reversal (range: 1 - 2)
+
+#The above also returns that the following birds do not have a criterion 1 for initial:
+
+#Cerveza - because originally passed despite not meeting fixed-window criterion i.e., did 7/10 and then 10/10 
+#Queso - because trial 24 originally skipped, so initial actually ends at trial 49 (not trial 50)
+
+#And that the following bird has two criterion 1 for initial:
+
+#Memela - because originally left in-test for an extra 11 trials, so meets sliding-window criterion twice
+
+#Moreover, the following birds are shown to not have a criterion 1 for reversal:
+
+#Adobo - because two trials where experimenter rewarded the wrong colour, were left in trial count but not actually considered valid choices; so reversal ends at trial 100 (and not trial 102)
+#Guacamole - this is expected because this bird was pulled from the experiment (see above)
+#Habenero - because trial 9 skipped, so reversal actually ends at trial 79 (not trial 80)
+#Huachinango - this is expected because this bird was pulled from the experiment (see above)
+#Marisco - because trial 13 skipped
+#Michelada - because originally passed despite not meeting the fixed-window criterion i.e., did 7/10 and then 10/10
+
+#Thus, aside from extra learning trials, original experimenter oversight extends to trial entry error and misapplying the learning criterion
+
+#Furtheremore, if we apply the sliding-window method used by experimenters on the edge population to our data, 
+#we find that these birds also gained extra learning trials due to experimenter oversight
+
+#Sliding-window Criterion
+dat$SW_Criterion <- NA #0 = not passed; 1 = passing trial; 2 = extra learning trials, either in initial or reversal 
+for(i in 1:nrow(dat)){
+  #Baseline for all rows
+  dat$SW_Criterion[i] <- 0 
   #Trial meet SW_Criterion
-  #Beginning at trial 18
-  if(dat$Trial[i] == 18){
-    if(sum(dat$Correct[(i - 17):i]) >= 17){
-      if((sum(dat$Correct[(i-17):(i-8)]) >= 8) & (sum(dat$Correct[(i-7):i]) == 8)){
-        dat$SW_Criterion[i] <- 1
-      }
-    }
-  }
-  #Moving to trial 19
-  if(dat$Trial[i] == 19){
-    if(sum(dat$Correct[(i - 18):i]) >= 17){
-      if((sum(dat$Correct[(i-18):(i-9)]) >= 8) & (sum(dat$Correct[(i-8):i]) == 8)){
-        dat$SW_Criterion[i] <- 1
-      }
-    }
-  }
-  #Any trial equal to or above 20
-  if(dat$Trial[i] >= 20){
+  if(dat$Trial[i] >= 20){ #Any trial equal to or above 20
     if(sum(dat$Correct[(i - 19):i]) >= 17){
       if((sum(dat$Correct[(i - 19):(i - 10)]) >= 8) & (sum(dat$Correct[(i - 9):i]) >= 8)){
         dat$SW_Criterion[i] <- 1
@@ -326,13 +369,13 @@ for(i in 1:nrow(dat)){
   #Extra trials beyond SW_Criterion 
   #Currently, the sheet will have multiple birds with multiple trials where SW_Criterion was met - need to only have first case
   #Can have a pass, followed by a no-pass, and then later on more passes - detect this
-  if(dat$Trial[i] >= 18){ #If trial 18 or bigger
+  if(dat$Trial[i] >= 20){ #If trial 18 or bigger
     if(dat$SW_Criterion[i - 1] == 1){ #If bird passed in previous trial, indicated by summing b/c 0 + 1 = 1
       dat$SW_Criterion[i] <- 2 #Assign 2
     }
   }
   #Now because first-pass trial will always be followed by a 2, it is possible to detect all cases post-initial pass, and assign 2
-  if(dat$Trial[i] >= 18){ #If trial 18 or bigger
+  if(dat$Trial[i] >= 20){ #If trial 18 or bigger
     if(sum(dat$SW_Criterion[(i-1):i]) >= 2){ #If bird already passed, indicated by summing b/c 1 + 2 = 3 (where 2 can be assigned from condition above or from previous loop through current statement) 
       dat$SW_Criterion[i] <- 2 #Assign 2
     }
@@ -341,9 +384,10 @@ for(i in 1:nrow(dat)){
 
 edge_oversight <- dat %>% filter(Population == 3) #Only edge pop birds
 table(edge_oversight$ID, edge_oversight$SW_Criterion, edge_oversight$Phase) #Returns 11 in initial (range: 1 - 10), 7 in reversal (range: 1 - 14)
-#Thus irrespective of the original learning method used by experimenters, birds experienced extra learning trials
 
-#For our supplementary Poisson, we need choice-option switch counts
+#All of the data issues and problematic nature of both the fixed- and sliding-window learning criterion, summarised above, remain unaddressed by the Grackle Project...
+
+#Anyway, for our choce-option-switch-count Poisson, we need switch counts
 dat$switch <- NA
 for(i in 1:nrow(dat)){
   if(dat$Trial[i] > 1){
